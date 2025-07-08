30b688687d0acdad97e46be093ec5b5417ce6987
lordofpugs51 <danieldjasper000@gmail.com>
2025-07-08 13:27:58 -0500

    on it way

---
+++ myGame/main.lua
@@ -0,0 +1,29 @@
+function love.load()
+    whale = love.graphics.newImage('image/whale.png')
+
+    startX = 0
+    startY = 0
+
+end
+
+function love.update(dt)
+    startY = startY + 20 * dt
+end
+
+
+function love.draw()
+    love.graphics.draw(whale, startX, startY)
+end
+
+function love.mousepressed(x, y, button, istouch)
+    if button == 1 then
+        print("leftMouseClicked")
+        if x >= startX and x <= startX + roosterFace:getWidth()
+        and y >= startY and y <= startY + roosterFace:getHeight() then
+         print("in bounds")
+        math.randomseed(os.time())
+        math.random(); math.random(); math.random()
+         startY = math.random (whale:getHeight(), whale:getHeight() * 2) * -1
+        end
+    end
+end
