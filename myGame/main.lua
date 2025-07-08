69318f77cb72cff78d03ebbdb091068e1eb8099c
lordofpugs51 <danieldjasper000@gmail.com>
2025-07-08 13:53:55 -0500

    Update main.lua

--- myGame/main.lua
+++ myGame/main.lua
@@ -25,7 +25,7 @@ function love.mousepressed(x, y, button, istouch)
         math.randomseed(os.time())
         math.random(); math.random(); math.random()
          startY = math.random (whale:getHeight(), whale:getHeight() * 0) * -1
-         startX = math.random (whale:getWidth(), whale:getWidth() * 0) * 1
+         startX = math.random (whale:getWidth(), whale:getWidth() * 0) * -1
         end
     end
 end
